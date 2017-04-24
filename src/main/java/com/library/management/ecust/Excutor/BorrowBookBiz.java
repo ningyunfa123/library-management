ddcd72eec2c91e8f6210f7c65bcba8cf32c8916b
ningyunfa123 <1063104204@qq.com>
2017-04-25 00:22:06 +0800

    borrow book

---
+++ src/main/java/com/library/management/ecust/Excutor/BorrowBookBiz.java
@@ -0,0 +1,7 @@
+package com.library.management.ecust.Excutor;
+
+/**
+ * Created by fage on 2017/4/24.
+ */
+public class BorrowBookBiz {
+}
---
+++ src/main/java/com/library/management/ecust/models/BorrowBookDTO.java
@@ -0,0 +1,7 @@
+package com.library.management.ecust.models;
+
+/**
+ * Created by fage on 2017/4/24.
+ */
+public class BorrowBookDTO {
+}
